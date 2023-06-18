924cf518a2b98c5fdd797a6d05d017f630756dc9
Ndigithe-john <61590347+Ndigithe-john@users.noreply.github.com>
2023-06-18 02:21:08 -0700

    Revert "Borrowing and returning books endPoint"

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require("express");
-require("dotenv").config();
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-const router = require("./src/routes/loansRoute");
-const port = process.env.PORT || 3030;
-app.use(express.json());
-app.use(router);
-app.use(bodyParser.json());
-app.listen(port, () => console.log(`Server running on port ${port}`));
+
+
+app.use (express.json());
+
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
+
+const port = 3000;
+
+app.listen(port, ()=>console.log(`Server running on port ${port}`))
