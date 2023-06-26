3c223a4f4ab00c41d5cbbe59fd0227fbb6762614
lucymutunga <lucyalphonce18@gmail.com>
2023-06-26 21:47:03 +0300

    merged the new folder structure

--- client/src/components/HomePage/GetAllProducts.jsx
+++ client/src/components/HomePage/GetAllProducts.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import Axios from "axios";
 
 const GetAllProducts = () => {
-  return <div>GetAllProducts</div>;
+  let token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNZW1iZXJJRCI6MTAwNCwicm9sZXMiOiJhZG1pbiIsImlhdCI6MTY4NzgwMjE1MywiZXhwIjoxNjg3ODA1NzUzfQ.U9cNaiPXFgZiy5p44vrUjv4Dzpj-viV0vIVUfZHBjVk";
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const getBooks = async () => {
+      try {
+        const response = await Axios.get("http://localhost:5050/books", {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        setBooks(response.data.results);
+        // console.log(response.data.results);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getBooks();
+  }, []);
+
+  return (
+    <div>
+      <section className="grid">
+        {books.map((book) => {
+          const { BookID, Title, Author, PublicationYear, Img } = book;
+          return (
+            <article key={BookID}>
+              <div className="container">
+                <img src={Img} alt="BookImg" />
+                <div className="about">
+                  <h3>{}</h3>
+                  <h4>
+                    Title: <span>{Title}</span>
+                  </h4>
+                  <h4>
+                    Author: <span>{Author}</span>
+                  </h4>
+                  <h4>
+                    PubYear: <span>{PublicationYear}</span>
+                  </h4>
+                </div>
+              </div>
+            </article>
+          );
+        })}
+      </section>
+    </div>
+  );
 };
 
 export default GetAllProducts;
